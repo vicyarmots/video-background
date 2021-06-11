9f90b6d3e0891255df0e652bbee6da46d955d8a6
vicyarmots <victor.yarmots@yandex.ru>
2021-06-11 20:38:59 +0300

    video-background

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainPage from "./pages/MainPage.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { MainPage },
+};
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <main>
+      <video-background
+        :src="video"
+        style=" height: 100vh;"
+        overlay="linear-gradient(45deg,#2a4ae430,#fb949e6b)"
+        class="video"
+      >
+        <nav>
+          <p>Home</p>
+          <p>Contact</p>
+          <p>About us</p>
+        </nav>
+        <h1 style="color: white; text-align: center;">
+          The world is yours
+        </h1>
+      </video-background>
+    </main>
+  </div>
+</template>
+
+<script>
+import VideoBackground from "vue-responsive-video-background-player";
+import video from "../assets/video.mp4";
+export default {
+  name: "MainPage",
+  components: { VideoBackground },
+  data() {
+    return {
+      video,
+    };
+  },
+};
+</script>
+
+<style scoped>
+main {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background: black;
+}
+
+nav {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-size: 20px;
+  width: 100%;
+  color: white;
+}
+.video {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: radial-gradient(
+    farthest-corner at center center,
+    #ce1a1a 0%,
+    #ffffff 100%
+  );
+  font-size: 30px;
+  font-family: Pompadur;
+}
+@font-face {
+  font-family: Pompadur; /* Имя шрифта */
+  src: url(../assets/18450.ttf); /* Путь к файлу со шрифтом */
+}
+</style>
